f5f7098690c1535ca42a96e7361cb1574dfad174
ramoliyaYug <mastermind57369@gmail.com>
2025-05-29 14:48:16 +0530

    cors fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client"
 import axios from "axios"
 import App from "./App"
 
-axios.defaults.baseURL = "https://tiffinbox-sqem.onrender.com"
+axios.defaults.baseURL = "https://tiffinbox-backend.onrender.com"
 
 axios.defaults.withCredentials = true
 
